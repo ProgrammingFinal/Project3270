e5ec37b8a305edd9e96e016653474c3569938d4a
mcgin <mcgin@DESKTOP-6QCP7GH>
2018-12-01 14:45:03 -0500

    Flight Class

---
+++ Test2/src/flight/Flight.java
@@ -0,0 +1,74 @@
+package flight;
+
+import java.util.Date;
+
+public class Flight {
+	
+	int numOfPassengers;
+	int flightNum;
+	//scheduled date and time, not current
+	String date;
+	String time;
+	
+	String departingCity;
+	String arrivingCity;
+	boolean reserve;
+	int maxPassengers;
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public int getNumOfPassengers() {
+		return numOfPassengers;
+	}
+	public int getFlightNum() {
+		return flightNum;
+	}
+	
+	public String getFromCity() {
+		return departingCity;
+	}
+	
+	public String getToCity() {
+		return arrivingCity;
+	}
+	
+	//check is flight is reserved or not
+	public boolean isReserved() {
+		return reserve;
+	}
+	
+	//set flight to reserved
+	public void setReserve() {
+		reserve = true;
+	}
+	
+	//get fight information
+	public String getFlightInfo(int flightNum, String date, String time, String departingCity, String arrivingCity) {
+		return "Flight Number is " + flightNum + ", date is " + date + ", time is " + time + ", departing from " + departingCity + ", arriving at " + arrivingCity; 
+	}
+	
+	//When a flight is booked, correct number of passengers
+	public void bookFlight() {
+		numOfPassengers = numOfPassengers + 1;
+	}
+	
+	public void setMaxPass (int maxPass) {
+		this.maxPassengers = maxPassengers;
+	}
+	public boolean isFull(int maxPassengers, int numOfPassengers) {
+		boolean isFull; 
+		if(numOfPassengers == maxPassengers || numOfPassengers >= maxPassengers) {
+			isFull = true;
+		} else {
+			isFull = false;
+		}
+		return isFull;
+	}
+}
+
