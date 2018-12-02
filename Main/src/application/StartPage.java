beba93b9ff4a5e1569ff23a9a64ece82633b554a
Student <Student@LAPTOP-MNKBRPQ9.attlocal.net>
2018-12-02 15:53:45 -0500

    flight table

--- Main/src/application/StartPage.java
+++ Main/src/application/StartPage.java
@@ -11,11 +11,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 
-public class Main extends Application {
+public class StartPage extends Application {
 	
+	Button button;
+	Button button2;
 	
-	
-    @Override
+	/* @Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
@@ -26,13 +27,36 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	} 
+	} */
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Airline Reservation System");
+		
+		button = new Button();
+		button.setText("Start");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Good click!");
+			}
+			
+		});
+		
+		
+		
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().addAll(button);
+		
+		Scene scene = new Scene (layout, 500, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 	
-		
-		
-		
+
 }
+
