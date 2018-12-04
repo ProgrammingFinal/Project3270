cc44e3793a3c0bba890512925b2a34f9e393178b
Koi <43917410+ProgrammingFinal@users.noreply.github.com>
2018-12-04 11:23:38 -0500

    password recovery

--- AirlineResSys/src/application/RecoverPassword.java
+++ AirlineResSys/src/application/RecoverPassword.java
@@ -1,13 +1,132 @@
 package application;
 
-import javafx.stage.Stage;
+import java.sql.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
 
-public class RecoverPassword {
+public class RecoverPassword extends Application implements EventHandler<ActionEvent> {
 
-	// starts stage
-	public void start(Stage primaryStage) {
+	public static String user = "";
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		primaryStage.setTitle("Recover Password");
+		primaryStage.setResizable(false);
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(10);
+		grid.setHgap(10);
+
+		Label passwordRecoverLabel = new Label("Recover Your Password");
+		GridPane.setConstraints(passwordRecoverLabel, 1, 0);
+		grid.setAlignment(Pos.TOP_CENTER);
+		GridPane.setHalignment(passwordRecoverLabel, HPos.CENTER);
+
+		Label usernameLabel = new Label("Enter Your Username Below");
+		GridPane.setConstraints(usernameLabel, 1, 1);
+		grid.setAlignment(Pos.TOP_CENTER);
+		GridPane.setHalignment(usernameLabel, HPos.CENTER);
+
+		TextField username = new TextField();
+		GridPane.setConstraints(username, 1, 2);
+		grid.setAlignment(Pos.CENTER);
+
+		Button recover = new Button("Recover Password");
+		GridPane.setConstraints(recover, 1, 4);
+		recover.setOnAction(e -> {
+
+			try {
+				// get a connection to the database
+				Connection myConn = DriverManager.getConnection(
+						"jdbc:mysql://127.0.0.1:3306/demo", "root",
+						"programmingfinal1");
+				// create a statement
+				Statement myStat = myConn.createStatement();
+				// execute a query
+				ResultSet myRs;
+				user = username.getText().trim();
+				String sqlUserCheck = "SELECT `username` FROM `flights`.`users` where username = '" + user + "'";
+				myRs = myStat.executeQuery(sqlUserCheck);
+
+				// Creates a variable for future checking
+				int count = 0;
+
+				while (myRs.next()) {
+
+					count = count + 1;
+
+				}
+				myRs.close();
+				myStat.close();
+				myConn.close();
+
+				// If user is in the database and the password is correct it it will take user
+				// to main page
+				if (count == 1) {
+
+					SecurityValidation recoveryPage = new SecurityValidation();
+					try {
+
+						recoveryPage.start(primaryStage);
+					} catch (Exception e1) {
+
+					}
+				}
+
+				else {
+					AlertBox.display("Incorrect Username", "There is no user with the username: " + user);
+
+				}
+
+			}
+
+			catch (Exception e1) {
+
+			}
+
+		});
+
+		Button login = new Button("Return To Log In");
+		GridPane.setConstraints(login, 1, 5);
+		login.setOnAction(e -> {
+			LogIn loginPage = new LogIn();
+			try {
+
+				loginPage.start(primaryStage);
+			}
+
+			catch (Exception e1) {
+			}
+		});
+
+		login.setMinWidth(150);
+		recover.setMinWidth(150);
+
+		Scene scene = new Scene(grid, 320, 350);
+
+		grid.getChildren().addAll(passwordRecoverLabel, usernameLabel, username, recover, login);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.centerOnScreen();
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
